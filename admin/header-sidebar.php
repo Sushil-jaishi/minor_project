066c13b66a40495471a4f57d012202389fdde3e5
Sushil Jaishi <sushiljaishi@gmail.com>
2024-07-09 13:24:16 +0545

    header-sidebar shift to version 1

--- admin/header-sidebar.php
+++ admin/header-sidebar.php
@@ -1,5 +1,10 @@
-<div id="side-bar">
+<div id="header">
     <img src="../assets/images/logo_with_text.png" alt="fwu logo with text" id="logo-with-text">
+    <div id="admin-dashboard">Admin Dashboard</div>
+    <img src="../assets/images/profile.png" alt="Profile" id="profile">
+</div>
+<div id="body">
+    <div id="side-bar">
     <ul>
         <li><img src="../assets/icons/dashboard.svg" alt="Dashboard"> Dashboard</li>
         <li id="side-bar-students"><img src="../assets/icons/user-solid.svg" alt="Students">Students</li>
@@ -7,12 +12,8 @@
         <li><img src="../assets/icons/file-solid.svg" alt="Notice">Notice</li>
         <li><img src="../assets/icons/file-signature-solid.svg" alt="Results">Results</li>
     </ul>
-</div>
-
-<div id="header-content">
-    <div id="header">
-        <div id="header-space"></div>
-        <div id="admin-dashboard-text">Admin Dashboard</div>
-        <img src="../assets/images/profile.png" alt="Profile" id="profile">
     </div>
-<!-- </div> this div must be closed after include this page -->
+    <div id="content">
+
+    <!-- </div> -->
+<!-- </div> -->
